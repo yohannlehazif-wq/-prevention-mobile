6e432becc2734cf42da3aae30192d2d64bf5fb34
yohannlehazif-wq <yohannlehazif@gmail.com>
2026-04-08 16:15:36 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,11 @@
+import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom/client";
 
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <h1 style={{ fontFamily: "Arial" }}>
+    ✅ Prévention Mobile (React)
+  </h1>
+);
+``
